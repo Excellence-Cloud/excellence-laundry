f09f544bdd8e4233ec34afc87faaac63a9e8dad3
GustavoDev021 <nebulosne@gmail.com>
2025-02-26 09:43:08 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -187,7 +187,7 @@ export default function Home(props) {
           <WhyUs />
           <Team />
           <About />
-          <Services />
+                {/*<Services />*/}
           <Packs />
           <Lavanderia />
           <Aluguel />
